683bacc4906e285beb996f0a966a3d89fdadff32
andreranito <actranito@gmail.com>
2017-10-11 13:48:28 +0200

    test

--- app/src/main/java/bookmanager/chalmers/edu/readwin/Launcher.java
+++ app/src/main/java/bookmanager/chalmers/edu/readwin/Launcher.java
@@ -1,13 +1,34 @@
 package bookmanager.chalmers.edu.readwin;
 
+import android.content.Context;
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class Launcher extends AppCompatActivity {
+
+    Button create;
+    Context context;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_launcher);
+
+        context= this;
+        create= (Button)findViewById(R.id.create);
+
+        create.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent login = new Intent(context, LoginActivity.class);
+                startActivity(login);
+            }
+        });
     }
+
+
+
 }
