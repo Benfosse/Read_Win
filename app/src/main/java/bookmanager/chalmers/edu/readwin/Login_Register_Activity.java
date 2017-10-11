46470539f6e10e2c1841665b95cf7ebf3fefa2e2
andreranito <actranito@gmail.com>
2017-10-11 15:53:43 +0200

    login/register

--- app/src/main/java/bookmanager/chalmers/edu/readwin/Login_Register_Activity.java
+++ app/src/main/java/bookmanager/chalmers/edu/readwin/Login_Register_Activity.java
@@ -1,18 +1,43 @@
 package bookmanager.chalmers.edu.readwin;
 
+import android.content.Context;
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
+import android.widget.Button;
 
 public class Login_Register_Activity extends AppCompatActivity {
+
+    Button login, register;
+    Context context= this;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login_register_);
+
+        login= (Button)findViewById(R.id.login_button);
+        register= (Button)findViewById(R.id.register_button);
+
+        login.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent login = new Intent(context, LoginActivity.class);
+                startActivity(login);
+            }
+        });
+
+       /* register.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent register = new Intent(context, )
+            }
+        });*/
+
     }
 
 }
