f8c920cf4ddd7e540bdbd82eda04a41fc43f8732
Benfosse <benjamin.fosse@isep.fr>
2017-10-11 10:52:48 +0200

    Initial commit

---
+++ app/src/main/java/bookmanager/chalmers/edu/readwin/LauncherGame.java
@@ -0,0 +1,13 @@
+package bookmanager.chalmers.edu.readwin;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+public class LauncherGame extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_launchergame);
+    }
+}
