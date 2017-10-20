7235ec6a50392dca3432e5c9903738d8c103893c
andreranito <actranito@gmail.com>
2017-10-20 17:16:28 +0200

    Changing questions

--- app/src/main/java/bookmanager/chalmers/edu/readwin/Multiple_QuestionActivity.java
+++ app/src/main/java/bookmanager/chalmers/edu/readwin/Multiple_QuestionActivity.java
@@ -1,8 +1,10 @@
 package bookmanager.chalmers.edu.readwin;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.ImageButton;
 import android.widget.RadioButton;
 import android.widget.TextView;
 
@@ -14,6 +16,8 @@ public class Multiple_QuestionActivity extends AppCompatActivity {
 
     RadioButton ans1, ans2, ans3, ans4;
     TextView question;
+    ImageButton next, prev;
+    int question_index;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -25,6 +29,8 @@ public class Multiple_QuestionActivity extends AppCompatActivity {
         ans3 = (RadioButton)findViewById(R.id.radioButton3);
         ans4 = (RadioButton)findViewById(R.id.radioButton4);
         question = (TextView)findViewById(R.id.textQuestion);
+        next = (ImageButton)findViewById(R.id.next_Button);
+        prev = (ImageButton)findViewById(R.id.prev_Button);
 
 
         question.setText(getIntent().getStringExtra("Question"));
@@ -34,6 +40,8 @@ public class Multiple_QuestionActivity extends AppCompatActivity {
         ans2.setText(options[1]);
         ans3.setText(options[2]);
         ans4.setText(options[3]);
+
+        question_index = getIntent().getIntExtra("index", -1);
 
         ans1.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -72,6 +80,16 @@ public class Multiple_QuestionActivity extends AppCompatActivity {
                 ans2.setChecked(false);
                 ans3.setChecked(false);
                 ans4.setChecked(true);
+            }
+        });
+
+        next.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent data = new Intent();
+                data.putExtra("index", question_index+1);
+                setResult(RESULT_OK, data);
+                finish();
             }
         });
 
--- app/src/main/java/bookmanager/chalmers/edu/readwin/QuestionsActivity.java
+++ app/src/main/java/bookmanager/chalmers/edu/readwin/QuestionsActivity.java
@@ -7,7 +7,11 @@ import java.util.List;
 import bookmanager.chalmers.edu.readwin.models.Question;
 import bookmanager.chalmers.edu.readwin.services.QuestionService;
 
+
 public class QuestionsActivity extends AppCompatActivity {
+
+    static final int RESULT_MULTIPLE = 0, RESULT_PAIR = 1, RESULT_ORDERING = 2;
+    int question_index=0;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -15,16 +19,16 @@ public class QuestionsActivity extends AppCompatActivity {
         setContentView(R.layout.activity_questions);
 
         List<Question> questions = new QuestionService().getBookQuestions(1);
-        int question_index=0;
 
         switch (questions.get(question_index).getType()) {
 
             case "Multiple":
                 Intent multiple = new Intent(QuestionsActivity.this, Multiple_QuestionActivity.class);
-                multiple.putExtra("index", questions.get(question_index).getQuestionNumber());
+                multiple.putExtra("index", question_index);
+                multiple.putExtra("question_number", questions.get(question_index).getQuestionNumber());
                 multiple.putExtra("Question", questions.get(question_index).getMultiple().getQuestion());
                 multiple.putExtra("Options", questions.get(question_index).getMultiple().getOptions());
-                startActivity(multiple);
+                startActivityForResult(multiple, RESULT_MULTIPLE);
 
             case "Pair":
 
@@ -34,5 +38,43 @@ public class QuestionsActivity extends AppCompatActivity {
 
         }
 
+    }
+
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        setContentView(R.layout.activity_questions);
+
+        List<Question> questions = new QuestionService().getBookQuestions(1);
+
+        switch (questions.get(question_index).getType()) {
+
+            case "Multiple":
+                Intent multiple = new Intent(QuestionsActivity.this, Multiple_QuestionActivity.class);
+                multiple.putExtra("index", question_index);
+                multiple.putExtra("question_number", questions.get(question_index).getQuestionNumber());
+                multiple.putExtra("Question", questions.get(question_index).getMultiple().getQuestion());
+                multiple.putExtra("Options", questions.get(question_index).getMultiple().getOptions());
+                startActivityForResult(multiple, RESULT_MULTIPLE);
+
+            case "Pair":
+
+            case "Ordering":
+
+            case "default":
+
+        }
+
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        if (resultCode == RESULT_OK && requestCode == RESULT_MULTIPLE) {
+            if (data != null) {
+                int temp = data.getIntExtra("index", -1);
+                question_index = temp;
+            }
+        }
     }
 }
